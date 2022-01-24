666dd8243f40fc242dd1cf84efd834b07afda837
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-24 10:41:51 -0300

    Criando function de Requisicao Usando Fetch

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ const starships = document.getElementById('starships');
 const planets = document.getElementById('planets');
 
 fillCounters();
+
+
+
+
+function getData(param) {
+  return fetch(`https://swapi.dev/api/${param}`)
+    .then(res => res.json())
+};
